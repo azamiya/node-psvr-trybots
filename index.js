8c7f4ed0ee3b5c7020e962e81ad6776bccdd4150
TATSUKI ADANIYA <tatsukiadaniya@tatsuki.local>
2016-10-30 11:10:10 +0900

    enable PSVR VR mode

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+const PORT = 4201;
+
+//http server
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const server = require("./server");
+app.use(express.static(__dirname + '/client'));
+
+server.start(app, http, PORT);
+
+//Connect to PSVR
+var PSVR = require("psvr");
+var device = new PSVR();
+
+device.on("data", function(data) {
+});
